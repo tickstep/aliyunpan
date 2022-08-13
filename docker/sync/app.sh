d0ad94aa65851ee7f6890cc55b48db48d45f3a7e
tickstep <tickstep@outlook.com>
2022-08-13 18:43:29 +0800

    add priority option for sync docker

--- docker/sync/app.sh
+++ docker/sync/app.sh
@@ -19,6 +19,7 @@ else
       "localFolderPath": "$ALIYUNPAN_LOCAL_DIR",
       "panFolderPath": "$ALIYUNPAN_PAN_DIR",
       "mode": "$ALIYUNPAN_SYNC_MODE",
+      "priority": "$ALIYUNPAN_SYNC_PRIORITY",
       "lastSyncTime": "2022-06-12 19:28:20"
      }
     ]
