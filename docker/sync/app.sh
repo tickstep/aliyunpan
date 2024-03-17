68b3a4aba6894c936b532df83f9e421312fe1a49
tickstep <tickstep@outlook.com>
2024-03-17 15:53:28 +0800

    update sync docker file

--- docker/sync/app.sh
+++ docker/sync/app.sh
@@ -15,11 +15,11 @@ else
     "syncTaskList": [
      {
       "name": "阿里云盘备份",
-      "id": "5b2d7c10-e927-4e72-8f9d-5abb3bb04814",
+      "id": "5b2d7c10-e927-4e72-8f9d-5abb3bb04815",
       "localFolderPath": "$ALIYUNPAN_LOCAL_DIR",
       "panFolderPath": "$ALIYUNPAN_PAN_DIR",
       "mode": "$ALIYUNPAN_SYNC_MODE",
-      "priority": "$ALIYUNPAN_SYNC_PRIORITY",
+      "driveName": "$ALIYUNPAN_SYNC_DRIVE",
       "lastSyncTime": "2022-06-12 19:28:20"
      }
     ]
@@ -30,13 +30,13 @@ fi
 sleep 2s
 
 # device-id
-if [[ -z $ALIYUNPAN_DEVICE_ID ]];
-then
-  echo "the program use random device id"
-else
-  echo "set device id"
-  ./aliyunpan config set -device_id ${ALIYUNPAN_DEVICE_ID}
-fi
+#if [[ -z $ALIYUNPAN_DEVICE_ID ]];
+#then
+#  echo "the program use random device id"
+#else
+#  echo "set device id"
+#  ./aliyunpan config set -device_id ${ALIYUNPAN_DEVICE_ID}
+#fi
 
 # check login already or not
 ./aliyunpan who
@@ -44,11 +44,9 @@ if [ $? -eq 0 ]
 then
   echo "cache token is valid, not need to re-login"
 else
-  echo "login use refresh token: ${ALIYUNPAN_REFRESH_TOKEN}"
-  ./aliyunpan login -RefreshToken=${ALIYUNPAN_REFRESH_TOKEN}
+  echo "token is invalid, please use the valid aliyunpan_config.json file and retry"
+#  ./aliyunpan login -RefreshToken=${ALIYUNPAN_REFRESH_TOKEN}
 fi
-
-./aliyunpan config set -transfer_url_type ${ALIYUNPAN_TRANSFER_URL_TYPE}
 
 if [ "$ALIYUNPAN_SYNC_LOG" = "true" ]
 then
@@ -57,4 +55,4 @@ else
   ./aliyunpan config set -file_record_config 2
 fi
 
-./aliyunpan sync start -dp ${ALIYUNPAN_DOWNLOAD_PARALLEL} -up ${ALIYUNPAN_UPLOAD_PARALLEL} -dbs ${ALIYUNPAN_DOWNLOAD_BLOCK_SIZE} -ubs ${ALIYUNPAN_UPLOAD_BLOCK_SIZE} -log ${ALIYUNPAN_SYNC_LOG} -ldt ${ALIYUNPAN_LOCAL_DELAY_TIME} -step ${ALIYUNPAN_TASK_STEP}
+./aliyunpan sync start -dp ${ALIYUNPAN_DOWNLOAD_PARALLEL} -up ${ALIYUNPAN_UPLOAD_PARALLEL} -dbs ${ALIYUNPAN_DOWNLOAD_BLOCK_SIZE} -ubs ${ALIYUNPAN_UPLOAD_BLOCK_SIZE} -log ${ALIYUNPAN_SYNC_LOG} -ldt ${ALIYUNPAN_LOCAL_DELAY_TIME}
