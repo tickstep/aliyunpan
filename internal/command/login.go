1f3131f71883e6f91942b364022ab95161fa8c3f
tickstep <tickstep@outlook.com>
2024-08-11 14:09:22 +0800

    remove useless prompt

--- internal/command/login.go
+++ internal/command/login.go
@@ -138,7 +138,7 @@ func RunLogin() (ticketId string, openapiToken, webapiToken *config.PanClientTok
 		fmt.Fprintf(loginUrl, "https://openapi.alipan.com/oauth/authorize?client_id=%s&redirect_uri=https%%3A%%2F%%2Fapi.tickstep.com%%2Fauth%%2Ftickstep%%2Faliyunpan%%2Ftoken%%2Fopenapi%%2F%s%%2Fauth2&scope=user:base,file:all:read,file:all:write",
 			config.Config.ClientId, ticketId)
 	}
-	fmt.Printf("请在浏览器打开以下链接进行登录，链接有效时间为5分钟。\n注意：你需要进行一次授权一次扫码的两次登录。\n%s\n\n", loginUrl)
+	//fmt.Printf("请在浏览器打开以下链接进行登录，链接有效时间为5分钟。\n注意：你需要进行一次授权一次扫码的两次登录。\n%s\n\n", loginUrl)
 
 	// handler waiting
 	line := cmdliner.NewLiner()
